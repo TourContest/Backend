d15a229ea3df97942ea1bbb7a37c1f12e16e564d
hyeonjune <annhj980@naver.com>
2025-06-08 21:48:15 +0900

    [FEAT] 초기 설정

---
+++ jejuday/src/main/java/com/goodda/jejuday/JejudayApplication.java
@@ -0,0 +1,13 @@
+package com.goodda.jejuday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejudayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JejudayApplication.class, args);
+    }
+
+}
---
+++ jejuday/src/test/java/com/goodda/jejuday/JejudayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodda.jejuday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejudayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
